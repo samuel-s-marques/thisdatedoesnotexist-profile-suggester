d581665c1367325957c9a35c02884ef946df7b7c
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-23 16:35:06 -0300

    Add find_similar_profiles_route to app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/find-similar-profiles', methods=['POST'])
+def find_similar_profiles_route():
+    try:
+        return jsonify({'message': 'Hello World!'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
